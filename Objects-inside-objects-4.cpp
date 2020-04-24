1223f3e4851b9c9bb92d2ec13b70c57182e1cb5c
Vatsilidis Lampros <vatsilidis@icloud.com>
2020-04-25 02:46:23 +0300

    different cases of objects inside other ones, combining more features of OOP's

---
+++ Objects-inside-objects-4.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+class Element {
+	int value;
+public:
+	Element(int val) { value = val; cout << "Element(" << val << ") constructed!" << endl; }
+	int Get(void) { return value; }
+	void Put(int val) { value = val; }
+};
+
+class Collection {
+	Element el1, el2;
+public:	
+	Collection(void) : el2(2), el1(1) { cout << "Collection constructed!" << endl; }
+	int Get(int elno) { return elno == 1 ? el1.Get() : el2.Get(); }
+	int Put(int elno, int val) { if(elno == 1) el1.Put(val); else el2.Put(val); }
+};
+
+int main(void) {
+	Collection coll;
+	return 0;
+}
