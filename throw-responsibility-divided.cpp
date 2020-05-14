8b46e27952ebeba728951916962b99d7f8eb175c
Vatsilidis Lampros <vatsilidis@icloud.com>
2020-05-14 20:52:21 +0300

    This is the last code of Exception handling lesson

---
+++ throw-responsibility-divided.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <exception>
+#include <stdexcept>
+
+using namespace std;
+
+void function(int i) {
+	switch(i) {
+	case 0: throw domain_error("0");
+	case 1: throw logic_error("1");
+	case 2: throw exception();
+	case 3: throw range_error("2");
+	case 4: throw "so bad";
+	}
+}
+
+void broker(int i) {
+	try {
+		function(i);
+	}
+	catch(...) {
+		cout << "Broker swept problems under the carpet " << endl;
+		throw;
+	}
+}
+
+int main(void) {
+	for(int i = 0; i < 5; i++) {
+		try {
+			broker(i);
+		}
+		catch(logic_error le) {
+			cout << "Logic error: " << le.what() << endl;
+		}
+		catch(runtime_error re) {
+			cout << "Runtime error: " << re.what() << endl;
+		}
+		catch(exception ex) {
+			cout << "Exception: " << ex.what() << endl;
+		}
+		catch(...) {
+			cout << "Something bad happened" << endl;
+		}
+	}
+	return 0;
+}
